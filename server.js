e795884a1c38526113fa6058d5612267b7dfee1b
Nate-Huerd <nhuerd3@gmail.com>
2022-02-10 14:03:45 -0600

    added db.query to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'P*bH^5h)_cJsj2>/2n^h',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
     });
   });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
